0c22148485964fe4ab5220491e6e084a873ed9fb
RaghaMruthikaM <mraghamruthika@gmail.com>
2026-03-02 21:08:22 +0530

    Fix render timeout

--- app.py
+++ app.py
@@ -339,5 +339,5 @@ def complaint_status_update(complaint_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
